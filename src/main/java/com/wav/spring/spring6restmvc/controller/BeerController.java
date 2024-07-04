d0e5fad7754b1e9360a05d09c5c86ad2129c5a92
willianvaler <willianvaler@gmail.com>
2024-07-04 19:37:33 -0300

    add header location on post request

--- src/main/java/com/wav/spring/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/wav/spring/spring6restmvc/controller/BeerController.java
@@ -4,6 +4,7 @@ import com.wav.spring.spring6restmvc.service.BeerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import com.wav.spring.spring6restmvc.model.Beer;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +34,10 @@ public class BeerController
     {
         Beer saved = beerService.saveNewBeer(beer);
 
-        return new ResponseEntity( HttpStatus.CREATED );
+        HttpHeaders headers = new HttpHeaders();
+        headers.add( "Location", "/api/b1/beer" + saved.getId().toString() );
+
+        return new ResponseEntity( headers, HttpStatus.CREATED );
     }
 
     @RequestMapping(method = RequestMethod.GET)
