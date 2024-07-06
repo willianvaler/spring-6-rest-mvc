9c5efd8d5526df696fccf79f89c1cb780e23ab3c
willianvaler <willianvaler@gmail.com>
2024-07-06 18:22:37 -0300

    centralize exception handling in one class

--- src/main/java/com/wav/spring/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/wav/spring/spring6restmvc/controller/BeerController.java
@@ -76,14 +76,6 @@ public class BeerController
         return beerService.listBeers();
     }
 
-    @ExceptionHandler( NotFoundException.class)
-    public ResponseEntity handleNotFound()
-    {
-        System.out.println("in controller handler");
-        
-        return ResponseEntity.notFound().build();
-    }
-
     @GetMapping(value = BEER_PATH_ID)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId)
     {
---
+++ src/main/java/com/wav/spring/spring6restmvc/controller/ExceptionController.java
@@ -0,0 +1,17 @@
+package com.wav.spring.spring6restmvc.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController
+{
+    @ExceptionHandler( NotFoundException.class)
+    public ResponseEntity handleNotFound()
+    {
+        System.out.println("in controller handler");
+
+        return ResponseEntity.notFound().build();
+    }
+}
